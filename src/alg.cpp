62b92b20530a2f908f4b7512e1978e84059d7033
yaroslavstepanov <75724049+yaroslavstepanov@users.noreply.github.com>
2021-04-18 13:00:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,70 +1,71 @@
 // Copyright 2021 NNTU-CS
-#include <string>
+#include <iostream>
 #include "tstack.h"
-
+using namespace std;
 
 int prior(char ch)
 {
-switch (ch)
-{
-case '(': return 0;
-case ')': return 1;
-case '+': return 2;
-case '*': return 3;
-case '/': return 3;
-default: return -1;
-}
+    switch (ch)
+    {
+    case '(': return 0;
+    case ')': return 1;
+    case '+': return 2;
+    case '-': return 2;
+    case '*': return 3;
+    case '/': return 3;
+    default: return -1;
+    }
 }
 
-std::string infx2pstfx( std::string inf) {
-std::string pfx;
-int i = 0;
-char ch = inf[i];
-char top = 0;
-TStack <char> stackCh;
-while (ch) {
-int prioritet;
-prioritet = prior(ch);
-if (prioritet > -1) {
-if ((prioritet == 0 || prioritet > prior(top) ||
-stackCh.isEmpty()) && ch != ')') {
-if (stackCh.isEmpty())
-top = ch;
-stackCh.push(ch);
-} else if (ch == ')') {
-while (stackCh.get() != '(') {
-pfx.push_back(stackCh.get());
-pfx.push_back(' ');
-stackCh.pop();
+string infx2pstfx(string inf)
+{
+    TStack<char> stack1;
+    string tmp = "";
+    for (int i = 0; i < inf.size(); i++)
+    {
+        char ch = inf[i];
+        int k = prior(ch);
+
+        if (k == -1)
+            tmp.append(string(1, ch));
+        else
+            if (stack1.isEmpty() || k == 0 || k > prior(stack1.get()))
+                stack1.push(ch);
+            else
+            {
+                if (ch == ')')
+                    while (true)
+                    {
+                        char sym = stack1.get();
+                        stack1.pop();
+                        if (sym != '(')
+                            tmp.append(string(1, sym));
+                        else
+                            break;
+                    }
+                else
+                {
+                    while (!stack1.isEmpty())
+                    {
+                        char lastStackEl = stack1.get();
+                        stack1.pop();
+                        if (prior(lastStackEl) >= k)
+                            tmp.append(string(1, lastStackEl));
+                    }
+                    stack1.push(ch);
+                }
+            }
+    }
+    while (!stack1.isEmpty())
+    {
+        char lastStackEl = stack1.get();
+        stack1.pop();
+        tmp.append(string(1, lastStackEl));
+    }
+    return tmp;
 }
-stackCh.pop();
-if (stackCh.isEmpty())
-top = 0;
-} else {
-while (!stackCh.isEmpty() && prior(stackCh.get()) >= prioritet) {
-pfx.push_back(stackCh.get());
-pfx.push_back(' ');
-stackCh.pop();
-}
-if (stackCh.isEmpty())
-top = ch;
-stackCh.push(ch);
-}
-} else {
-pfx.push_back(ch);
-pfx.push_back(' ');
-}
-ch = inf[++i];
-}
-while (!stackCh.isEmpty()) {
-pfx.push_back(stackCh.get());
-pfx.push_back(' ');
-stackCh.pop();
-}
-pfx.erase(pfx.end() - 1, pfx.end());
-return pfx;
-}
-int calculator(int k1, int k2, char pst)
+
+int excute_calc(int k1, int k2, char pst)
 {
     switch (pst)
     {
@@ -76,8 +77,9 @@ int calculator(int k1, int k2, char pst)
     }
 }
 
-int eval(std::string pst) {
-     TStack<int> stack2;
+int eval(string pst)
+{
+    TStack<int> stack2;
     for (int i = 0; i < pst.size(); i++)
     {
         char ch = pst[i];
@@ -93,7 +95,7 @@ int eval(std::string pst) {
             int k2 = stack2.get();
             stack2.pop();
 
-            int res = calculator(k2, k1, ch);
+            int res = excute_calc(k2, k1, ch);
             stack2.push(res);
         }
 
